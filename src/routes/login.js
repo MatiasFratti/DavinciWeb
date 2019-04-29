17f2445a2ec70ab56f74f1932023647afa4dedb1
Matias <mfrattibritos@gmail.com>
2019-04-28 23:56:05 -0300

    login para borrar

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.get('/login_admin',(req,res)=>{
-    if(req.session.name!='undefined'||req.session.name!=''){
-        res.status(404);
-    }
-    res.render('login',{
-        title:'Inicia sesión'
-    });
-});
+// router.get('/login_admin',(req,res)=>{
+//     if(req.session.name!='undefined'||req.session.name!=''){
+//         res.status(404);
+//     }
+//     res.render('login',{
+//         title:'Inicia sesión'
+//     });
+// });
 
